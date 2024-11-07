18b1430e90d3654047f538a55de7b8635f9114af
Assassin187 <1248308178@qq.com>
2024-11-07 15:57:10 +0800

    add fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+"""生成一个斐波那契数列
+
+Input:
+    输入一个正整数
+
+Output:
+    指定位置的斐波那契数组
+
+"""
+
+array = [0, 1]
+
+def fibonacci():
+    index = int(input("Enter index: "))
+    if index < 2:
+        print(array[index])
+    else:
+        while index != len(array):
+            array.append(array[-1] + array[-2])
+    for i in range(len(array)):
+        print(array[i])
+
+if __name__ == '__main__':
+    fibonacci()
