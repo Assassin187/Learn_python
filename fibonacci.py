69e62073c2ada558a88a27c59c2c8342e53c8f6e
Assassin187 <1248308178@qq.com>
2024-11-14 18:53:47 +0800

    refresh fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,12 @@
-"""生成一个斐波那契数列
+def fibonacci(array):
+    """生成一个斐波那契数列
 
-Input:
-    输入一个正整数
+    Input:
+        输入一个正整数
 
-Output:
-    指定位置的斐波那契数组
-
-"""
-
-array = [0, 1]
-
-def fibonacci():
+    Output:
+        指定位置的斐波那契数组
+    """
     index = int(input("Enter index: "))
     if index < 2:
         print(array[index])
@@ -21,4 +17,5 @@ def fibonacci():
         print(array[i])
 
 if __name__ == '__main__':
-    fibonacci()
+    array = [0, 1]
+    fibonacci(array)
